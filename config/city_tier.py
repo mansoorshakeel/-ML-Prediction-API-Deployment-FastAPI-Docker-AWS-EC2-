e3e9def234b8fe13de3cc18726c75c15c50dea10
Mansoor Shakeel <mansoorshakeel0@gmail.com>
2026-04-26 16:43:52 +0500

    Initial commit

---
+++ config/city_tier.py
@@ -0,0 +1,9 @@
+tier_1_cities = [
+    "Bahawalpur", "Rawalpindi", "Karachi", "Islamabad",
+    "Gujranwala", "Multan", "Mardan"
+]
+
+tier_2_cities = [
+    "Quetta", "Lahore", "Peshawar", "Sukkur",
+    "Sargodha", "Faisalabad", "Sialkot"
+]
